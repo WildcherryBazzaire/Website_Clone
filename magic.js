da457c8c6ba1ee4f324f06dbfb652c6611178a59
WildCherryBazzar <Froggyfree86@gmail.com>
2017-09-19 22:08:00 -1000

    I worked way to long on this

---
+++ magic.js
@@ -0,0 +1,12 @@
+console.log("sane");
+function lucky() {
+  window.location = "https://www.google.com/search?q=Oh!+You+Pretty+Things"
+}
+
+/*function search(keyPressed) {
+  var enterKey = keyPressed.event || keyPressed.keyCode;
+  if(enterKey == 13){
+    var x = document.getElementByTagName("input").value;
+    console.log(x);
+  }
+} work on this later */
